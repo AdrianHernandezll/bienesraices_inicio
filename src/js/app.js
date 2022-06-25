11a7f67eee3fbbb37f885bbdd251bb775e028e04
Adrian Hernandez <danix08@live.com>
2022-06-25 00:51:08 -0300

    menu hamburguesa y estilos

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize the page
+  eventenListeners();
+});
+
+function eventenListeners() {
+  // Add event listeners to the document
+}
