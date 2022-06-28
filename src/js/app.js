8dc8879197074e1616982c09644264409edda2c3
Adrian Hernandez <danix08@live.com>
2022-06-27 23:04:15 -0300

    Funcion click js

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Initialize the page
   eventListeners();
 });
 
 function eventListeners() {
-  // Add event listeners to the document
-  const menuMobile = document.querySelector(".mobile-menu");
+  let mobileMenu = document.querySelector(".mobile-menu");
 
-  menuMobile.addEventListener(" click ", navegacionResponsive);
+  mobileMenu = addEventListener("click", navegacionResponsive);
 }
 
 function navegacionResponsive() {
-  const navegacion = document.querySelector(".navegacion");
-
-  navegacion.classList.toggle("mostrar");
+  console.log("navegacion");
 }
