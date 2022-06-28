c6a3593fa8213d004f17851da2494e6420c656f9
Adrian Hernandez <danix08@live.com>
2022-06-27 22:39:24 -0300

    index y min.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize the page
-  eventenListeners();
+  eventListeners();
 });
 
-function eventenListeners() {
+function eventListeners() {
   // Add event listeners to the document
+  const menuMobile = document.querySelector(".mobile-menu");
+
+  menuMobile.addEventListener(" click ", navegacionResponsive);
+}
+
+function navegacionResponsive() {
+  const navegacion = document.querySelector(".navegacion");
+
+  navegacion.classList.toggle("mostrar");
 }
